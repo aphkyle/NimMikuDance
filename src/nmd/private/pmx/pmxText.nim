282c0fa1d55e4e26b9d51a69aef5f075763171d4
aph <81857274+aphkyle@users.noreply.github.com>
2022-04-23 21:17:11 +0800

    idk how to rename dirs

--- src/nmd/private/pmx/pmxText.nim
+++
@@ -1,6 +0,0 @@
-import std/[streams, encodings]
-
-proc readText*(s: Stream, encoding: bool): string =
-  let textSize = s.readInt32()
-  let text = s.readStr(textSize)
-  return if encoding: text else: text.convert("utf-8", "utf-16")
